84b4d23c73aec934acbe68e99f7dc75820142bae
Rafael Santana <rafaelns.br@gmail.com>
2022-05-22 20:28:51 -0300

    Criando o projeto e adicionando dependências

---
+++ app/src/main/java/com/example/rogalabsteste/PostDisplayActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rogalabsteste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostDisplayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_display)
+    }
+}
